fbb99fed90cbec21c60b2da86e4ad46215a86e79
AyaSasaki0403 <168701750+AyaSasaki0403@users.noreply.github.com>
2024-06-21 12:27:23 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let currentQuestion = 0;
+
+function showQuestion(index) {
+    const questions = document.querySelectorAll('.question');
+    questions.forEach((question, i) => {
+        question.style.display = (i === index) ? 'block' : 'none';
+    });
+}
+
+function nextQuestion() {
+    const questions = document.querySelectorAll('.question');
+    if (currentQuestion < questions.length - 1) {
+        currentQuestion++;
+        showQuestion(currentQuestion);
+    }
+}
+
+function prevQuestion() {
+    if (currentQuestion > 0) {
+        currentQuestion--;
+        showQuestion(currentQuestion);
+    }
+}
+
+showQuestion(currentQuestion);
